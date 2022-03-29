231116fa3280b39dae0a8c1911b5a6d73de1ead8
Gavrus97 <gavrus1997@gmail.com>
2022-03-29 12:32:14 +0200

    Test

--- 2022.03.29_cuts/src/Cuts.java
+++ 2022.03.29_cuts/src/Cuts.java
@@ -1,12 +1,11 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Cuts {
 
     public int countMaxIntersectedCuts(List<Cut> cuts) {
         List<Point> points = convertToPoints(cuts);
-        Collections.sort(points);
 
         int max = 0;
         int cutsCounter = 0;
@@ -14,10 +13,10 @@ public class Cuts {
         for (Point point : points) {
             if (point.isLeft) {
                 cutsCounter++;
-            } else {
                 if (cutsCounter > max) {
                     max = cutsCounter;
                 }
+            } else {
                 cutsCounter--;
             }
         }
@@ -25,12 +24,9 @@ public class Cuts {
     }
 
     private List<Point> convertToPoints(List<Cut> cuts) {
-        List<Point> result = new ArrayList<>();
-
-        for (Cut cut : cuts) {
-            result.add(new Point(cut.left, true));
-            result.add(new Point(cut.right, false));
-        }
-        return result;
+        return cuts.stream()
+                .flatMap(cut -> Stream.of(new Point(cut.left, true), new Point(cut.right, false)))
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
