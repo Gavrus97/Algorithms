273c50155f04f62da6b83ff91b2d2e67c08d1267
Gavrus97 <gavrus1997@gmail.com>
2022-05-03 12:55:38 +0200

    hw solution

---
+++ 2022.05.02_min_points_for_cuts/src/CutServiceTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CutServiceTest {
+
+    CutService sol = new CutService();
+
+    @Test
+    public void test_minPointsCoveredByCuts1(){
+        List<Cut> cuts = Arrays.asList(new Cut(2, 9),
+                new Cut(4, 13),
+                new Cut(6, 12),
+                new Cut(11, 19),
+                new Cut(21, 24));
+        assertEquals(3, sol.getMinPointsCoveringCuts(cuts));
+    }
+
+    @Test
+    public void test_minPointsCoveredByCuts2(){
+        List<Cut> cuts = Arrays.asList(new Cut(2, 9),
+                new Cut(4, 13),
+                new Cut(6, 12),
+                new Cut(15, 19),
+                new Cut(21, 24));
+        assertEquals(3, sol.getMinPointsCoveringCuts(cuts));
+    }
+
+    @Test
+    public void test_minPointsCoveredByCuts3(){
+        List<Cut> cuts = Arrays.asList(new Cut(2, 9),
+                new Cut(4, 13),
+                new Cut(6, 12),
+                new Cut(11, 19),
+                new Cut(17, 24));
+        assertEquals(2, sol.getMinPointsCoveringCuts(cuts));
+    }
+
+}
